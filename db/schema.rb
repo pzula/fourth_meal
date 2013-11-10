379585aa0cc90a9d2752f7af1c80338c8c952c82
edatrix <lespath@gmail.com>
2013-11-10 16:51:37 -0700

    installed gem paperclip to add images to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107205456) do
+ActiveRecord::Schema.define(version: 20131110232636) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -32,7 +32,11 @@ ActiveRecord::Schema.define(version: 20131107205456) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "active",      default: true
+    t.boolean  "active",             default: true
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "order_items", force: true do |t|
