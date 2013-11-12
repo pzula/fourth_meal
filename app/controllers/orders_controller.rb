0c89f93cdd6c5576126787a23a1a80b3f1483286
Will Mitchell <wvmitchell@gmail.com>
2013-11-12 15:12:00 -0700

    Added gem to make use of AWS and necessary config for production

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,9 +7,6 @@ class OrdersController < ApplicationController
   def new
     order = Order.create
     cookies[:order_id] = order.id
-    item = Item.find(params[:item_id])
-    order.items << item
-    flash.notice = order.items.last.title + " added to cart!"
     redirect_to items_path
   end
 
