ace674162a007a04a23dff430b607e0cc02c83c9
Will Mitchell <wvmitchell@gmail.com>
2013-11-13 20:18:14 -0700

    Catagory tags in items indexnow link to all items of that category

--- test/features/can_view_categories_test.rb
+++ test/features/can_view_categories_test.rb
@@ -12,4 +12,22 @@ class CanViewCategoriesTest < Capybara::Rails::TestCase
     assert_content page, "brunch"
   end
 
+  test "can click tag to see all items with that tag" do
+    category = Category.create(name: "brunch")
+    category2 = Category.create(name: "plates")
+    item = Item.create(title: 'Deviled Eggs', description: '12 luscious eggs', price: '1')
+    item2 = Item.create(title: 'Ham Sandwich', description: 'Pretty basic', price: '2')
+    category.items << item
+    category2.items << item2
+
+    visit root_path
+
+    within("#item_1") do
+      click_on "brunch"
+    end
+
+    refute_content page, "Ham Sandwich"
+
+  end
+
 end
