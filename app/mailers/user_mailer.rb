22c62c78cdb86dcd994e91bd6adab063f8a95a5d
Will Mitchell <wvmitchell@gmail.com>
2013-11-13 19:51:04 -0700

    Order complete email feature added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,12 @@ class UserMailer < ActionMailer::Base
     @url = "platable.herokuapp.com/login"
     mail(to: @user.email, subject: "Welcome to Platable")
   end
+
+  def order_email(user, order)
+    @user = user
+    @order = order
+    @items = order.items
+    @url = "platable.herokuapp.com"
+    mail(to: @user.email, subject: "Your Grub is Forthcoming!")
+  end
 end
