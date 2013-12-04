13e37e2615239db815b2f2690b94b05024863c80
Meeka <rrgayhart@gmail.com>
2013-12-04 16:55:55 -0700

    Finishes integration test for home page

--- test/integration/home_page_integration.rb
+++ test/integration/home_page_integration.rb
@@ -10,10 +10,9 @@ class HomePageIntegrationTest < Capybara::Rails::TestCase
     assert_content page, "Sign In"
 
     assert_content page, "Satisfy Your Craving"
-
     assert_content page, "Pick a Restaurant"
-
     assert_content page, "Restaurant Owner?"
+
   end
 
   test "Clicking on food type icon brings up restaurants" do
@@ -32,4 +31,86 @@ class HomePageIntegrationTest < Capybara::Rails::TestCase
 
     assert_content page, "Platable" 
     refute_content page, "Dive Bar"
+
+  end
+
+  test "Clicking on sign up redirects to sign up form" do
+
+    visit root_path
+
+    click_on "Sign Up"
+
+    assert_content page, "Confirm Password:"
+
+  end
+
+  test "Clicking on sign in redirects to sign in form" do
+
+    visit root_path
+    click_on "Sign In"
+
+    assert_content page, "Email:"
+    refute_content page, "Confirm Password:"
+
+  end
+
+  test "Signing in remove Sign Up from home page" do
+
+    user1 = User.new({username: 'regular', password: 'password'})
+    assert user1.save
+
+    visit root_path
+    click_on "Sign In"
+    
+    fill_in "Username", with: 'admin'
+    fill_in "Password", with: 'password'
+
+    click_button "Sign In"
+    assert_content page, "Satisfy"
+    refute_content page, 'Sign In'
+
+  end
+
+  test "Choosing join craveyard redirects to sign up and join form"
+    
+    visit root_path
+    click_on "Join Craveyard"
+
+    assert_content page, "Sign In Now"
+    assert_content page, "Join"
+
+  end
+
+  test "Signing in Redirects to New Restaurant Application"
+
+    user1 = User.new({username: 'regular', password: 'password'})
+    assert user1.save
+
+    visit root_path
+    click_on "Join Craveyard"
+
+    fill_in "Username", with: 'admin'
+    fill_in "Password", with: 'password'
+
+    click_button "Sign In"
+
+    assert_content page, "New Restaurant Application"
+
+  end
+
+  test "Signing Up Redirects to New Restaurant Application"
+
+    visit root_path
+    click_on "Join Craveyard"
+
+    fill_in "Username", with: 'admin'
+    fill_in "Password", with: 'password'
+    fill_in "Password Confirmation", with: 'password'
+
+    click_button "Sign Up"
+
+    assert_content page, "New Restaurant Application"
+
+  end
+
 end
