dd136b4f88b25549bb496f5d215b930eba9fe75d
Meeka <rrgayhart@gmail.com>
2013-12-04 19:20:21 -0700

    Sets up initial item spec test

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Item do
+  it "has method active that returns actice items" do
+    Item.create({title: 'tasty', description: '3', price:'bread'})
+    Item.create({title: 'smelly', description: '5', price:'3', active: false})
+
+    expect(Item.active.count).to eq(1)
+  end
+
+  it "can attach image when editing" do
+    @item = Item.new({title: 'eggs', description: 'tasty', price: '3'})
+    @item.image = File.new("test/fixtures/deviled_eggs.jpg")
+    @item.save
+    expect(@item.image_file_name).to eq("deviled_eggs.jpg")
+  end
+#Validations testing needs to be added
+end
