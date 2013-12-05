e93a3214704c95252dec8392adf607ea54d8fbdd
Meeka <rrgayhart@gmail.com>
2013-12-05 14:35:55 -0700

    Finishes failing rspec test for cart... wooooooo

--- spec/features/checkout_features_spec.rb
+++ spec/features/checkout_features_spec.rb
@@ -1,26 +1,26 @@
 require 'spec_helper'
 
 describe "Adding an item to the cart", :type => :feature do 
-  # before :each do
-      #item1 = Item.create(title: "Deviled Eggs", description: "twelve luscious eggs", price: 1)
-     # item2 = Item.create(title: "Waffles", description: "syrup", price: 3)
-     # item3 = Item.create(title: "quinoa", description: "grainy", price: 9)
-   # end
+  before :each do
+    item1 = Item.create(title: "Deviled Eggs", description: "twelve luscious eggs", price: 1)
+    item2 = Item.create(title: "Waffles", description: "syrup", price: 3)
+    item3 = Item.create(title: "quinoa", description: "grainy", price: 9)
+  end
 
   it "updates the cart without redirecting" do 
 
-    visit root_path
+    visit "/platable/menu"
 
-   # expect(page).to have_text("Deviled Eggs")
-   # within("#item_2") do 
-   #   click_on "Add to Order"
-   # end
-   # within("#item_2") do 
-   #   click_on "Add to Order"
-   # end
-   # expect(page).to have_text("My Order (2)")
-   # current_url.should == "/" 
-   expect(page).to have_content "Craveyard" 
+     expect(page).to have_text("Deviled Eggs")
+     within("#item_2") do 
+       click_on "Add to Order"
+     end
+     within("#item_2") do 
+       click_on "Add to Order"
+     end
+    
+    expect(page).to have_text("My Order (2)")
+    current_url.should == "/platable/menu" 
 
   end
 end
