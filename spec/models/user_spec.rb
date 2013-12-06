f9c7a12c11b059bbc2cdf09969c21231d2f2d0a1
Persa Zula <persa@persazula.com>
2013-12-06 14:34:35 -0700

    Updating user spec with old minitest specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,35 @@
 require 'spec_helper'
 
 describe User do
+ 
+  before :each do 
+    @user = FactoryGirl.create(:user)
+    @order = FactoryGirl.create(:order, user: @user)
+  end
 
   it "should have orders" do
-    user = FactoryGirl.create(:user)
-    order = FactoryGirl.create(:order, user: user)
-    user.orders.should include(order)
+    @user.orders.should include(@order)
+  end
+
+  it "is not an admin by default" do
+    @user.should_not be(@user.admin)
+  end
+
+  it "can be made to be an admin" do 
+    user = FactoryGirl.create(:user, admin: true)
+    user.admin.should be(true)
+  end
+
+  it "can complete an order" do
+    @user.change_order_to_completed
+    @user.orders.last.status.should eq("completed")
+  end
+
+  it "can find it's recent completed orders" do
+    order = FactoryGirl.create(:order, user:@user, status: 'completed')
+    @user.recent_orders.each do |order|
+      expect(order.status).to eq('completed')
+    end
   end
 
 end
