faf9848738aa99d732b905a809b47da7aa5e7f4d
Meeka <rrgayhart@gmail.com>
2013-12-06 13:25:57 -0700

    continues with order spec for models

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
-describe OrderItem do
+describe Order do
+
+  it "should find pending orders" do
+    expect(Order.all.count).to eq(0)
+    user = FactoryGirl.create(:user)
+    order = FactoryGirl.create(:order, user: user)
+    expect(Order.all.count).to eq(1)
+    expect(Order.pending.count).to eq(1)
+    order = FactoryGirl.create(:order, user: user, status: "paid")
+    expect(Order.all.count).to eq(2)
+    expect(Order.pending.count).to eq(1)
+  end
+
+  it "should hold items" do
+    user = FactoryGirl.create(:user)
+    order = FactoryGirl.create(:order, user: user)
+  end
   
 end
