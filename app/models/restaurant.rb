0f4c7931879961e0330dab6bfb18643850b76261
Tyler Long <tyler.stephen.long@gmail.com>
2013-12-09 14:30:21 -0700

    fix seed data and restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3}
-  validates :status, presence: true
+  #validates :status, presence: true
   validates :url_slug, presence: true, format: {with: /[A-Za-z0-9\-]+/, message: "Only use letters, numbers and -"}, uniqueness: {case_sensitive: false}
-  validates :food_type, presence: true
+  #validates :food_type, presence: true
   before_save {|restaurant| restaurant.url_slug.downcase!}
   has_many :items
 
