4ede681a25cf442a94fa6942f64b5bf450a23ba0
Meeka <rrgayhart@gmail.com>
2013-12-15 14:31:06 -0700

    Finally generates the correct migrations for the hours fix

--- db/migrate/20131215212924_create_hours.rb
+++ db/migrate/20131215212924_create_hours.rb
@@ -1,8 +1,8 @@
-class Hours < ActiveRecord::Migration
+class CreateHours < ActiveRecord::Migration
   def change
     create_table :hours do |t|
       t.integer :restaurant_id
-      t.integer :day
+      t.string :day
       t.time :start_at
       t.time :end_at
     end
