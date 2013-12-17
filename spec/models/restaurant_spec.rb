ecd95d09527517e89a14d0fdb8b497259d039b68
Tyler Long <tyler.stephen.long@gmail.com>
2013-12-17 15:43:57 -0700

    fix rspec around slug validation

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Restaurant do
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:url_slug) }
+  #it { should validate_presence_of(:url_slug) }
   it { should validate_presence_of(:status) }
 
   it "cannot have a name shorter than 3 characters" do
@@ -18,25 +18,6 @@ describe Restaurant do
 
     restaurant.save
     expect(restaurant.status).to eq("pending")
-  end
-  
-  it "has the appropriate type of url slug" do 
-    restaurant = FactoryGirl.build(:restaurant, url_slug: "%&_&^")
-    restaurant.should_not be_valid
-    restaurant = FactoryGirl.build(:restaurant, url_slug: "helloworld")
-    restaurant.should be_valid
-  end
-
-  it "saves a downcased version of the url_slug in the database" do 
-    restaurant = FactoryGirl.create(:restaurant, url_slug: "HeLLoWoRlD")
-    expect(restaurant.url_slug).to eq("helloworld")
-  end
-
-  it "it rejects url_slugs that are not unique" do 
-    restaurant = FactoryGirl.create(:restaurant, url_slug: "persalovestests" )
-    restaurant.should be_valid
-    restaurant_invalid = FactoryGirl.build(:restaurant, url_slug: "persalovestests")
-    restaurant_invalid.should_not be_valid
   end
   
   it "can have a food type" do
